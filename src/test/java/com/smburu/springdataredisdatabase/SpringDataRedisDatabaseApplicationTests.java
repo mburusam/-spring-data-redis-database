e1d48fc62d58a4834c99e21442a32eecce65ee44
Samuel Mburu <samuel.mburu@tracom.co.ke>
2022-10-20 23:11:49 +0300

    initial commit

---
+++ src/test/java/com/smburu/springdataredisdatabase/SpringDataRedisDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smburu.springdataredisdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRedisDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
